9fd8196842a0cef66db29ee617a6fd8c03d416e7
chavangayatri-c <chavangayatri692@gmail.com>
2026-01-21 11:53:06 +0530

    Create train_models.py

---
+++ src/src/train_models.py
@@ -0,0 +1,13 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+
+def get_models():
+    models = {
+        "Logistic Regression": LogisticRegression(),
+        "Support Vector Machine": SVC(probability=True),
+        "Random Forest": RandomForestClassifier(n_estimators=100, random_state=42),
+        "XGBoost": XGBClassifier(use_label_encoder=False, eval_metric="logloss")
+    }
+    return models
